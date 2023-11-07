e23ebd060eaf4ffca4ec2d5f668c54baf968c18c
Carlos Andrino <cfandrino@gmail.com>
2023-11-07 09:31:45 -0600

    Prueba

--- PAT_HojaDeTrabajo02/PAT_HojaDeTrabajo02.cpp
+++ PAT_HojaDeTrabajo02/PAT_HojaDeTrabajo02.cpp
@@ -1,58 +1,19 @@
+// ConsoleApplication1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
 #include <iostream>
-#include "Ejercicio01.h"
-#include "Ejercicio02.h"
-#include "Ejercicio03.h"
-#include "Ejercicio04.h"
-#include "Node.h"
 
-int main() {
-
-    Ejercicio01 e01;
-
-    Node<char>* head = new Node<char>('a');
-    head->next = new Node<char>('b');
-    head->next->next = new Node<char>('c');
-    head->next->next->next = new Node<char>('d');
-    head->next->next->next->next = new Node<char>('e');
-    int k = 2;
-
-    Node<char>* rotatedList = e01.rotateRight(head, k);
-
-    std::cout << "Resultado de la rotación de lista:" << std::endl;
-    while (rotatedList) {
-        std::cout << rotatedList->value << " -> ";
-        rotatedList = rotatedList->next;
-    }
-    std::cout << "nullptr" << std::endl;
-
-    Ejercicio02 e02;
-
-    Node<int>* l1 = new Node<int>(2);
-    l1->next = new Node<int>(4);
-    l1->next->next = new Node<int>(3);
-
-    Node<int>* l2 = new Node<int>(5);
-    l2->next = new Node<int>(6);
-    l2->next->next = new Node<int>(4);
-
-    Node<int>* sumList = e02.addTwoNumbers(l1, l2);
-
-    std::cout << "Resultado de la suma de números en listas enlazadas:" << std::endl;
-    while (sumList) {
-        std::cout << sumList->value;
-        if (sumList->next) {
-            std::cout << " -> ";
-        }
-        sumList = sumList->next;
-    }
-    std::cout << std::endl;
-
-    Ejercicio03 e03;
-
-    std::string expression = "3+2*2";
-    int result = e03.calculate(expression);
-    std::cout << "Resultado de la evaluación de expresión: " << result << std::endl;
-
-    
-    return 0;
+int main()
+{
+    std::cout << "Hello World!\n";
 }
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
